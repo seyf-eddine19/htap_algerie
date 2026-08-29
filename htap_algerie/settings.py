748e0bc8a72ff602bd055f296213bb2dfc4cecca
seyfeddine-19 <seyfeddine.tlm@gmail.com>
2026-08-29 18:00:48 +0100

    Update settings

--- htap_algerie/settings.py
+++ htap_algerie/settings.py
@@ -110,8 +110,9 @@ elif DATABASES["default"]["ENGINE"] == "django.db.backends.mysql":
             "HOST": env("DB_HOST", default="127.0.0.1"),
             "PORT": env("DB_PORT", default="3306"),
             "OPTIONS": {
-                "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
                 "charset": "utf8mb4",
+                "use_unicode": True,
+                "init_command": "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci, sql_mode='STRICT_TRANS_TABLES'",
             },
         }
     )
